d769c2691adbe60220648e95b3f0270b7fabde58
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-05-08 23:31:00 -0400

    Added tests for connect four and made game run

--- spec/tdd_connect_four_spec.rb
+++ spec/tdd_connect_four_spec.rb
@@ -1,38 +1,128 @@
 require_relative '../tdd_connect_four'
 
 describe ConnectFourBoard do
-	describe '#check_win' do
-		let(:board) { described_class.new }
-		context 'when there is a horizontal line win' do
-			it 'returns true' do
-				board.cells[0][0] = board.cells[0][1] = board.cells[0][2] = board.cells[0][3] = 'X'
-				expect(board.check_win).to be true
-			end
-		end
+  
+  describe '#check_win' do
+    let(:board) { described_class.new }
+    context 'when there is a horizontal line win' do
+      it 'returns true' do
+        board.cells[0][0] = board.cells[0][1] = board.cells[0][2] = board.cells[0][3] = 'X'
+        expect(board.check_win).to be true
+      end
+    end
 
-		context 'when there is not a horizontal line win' do
-			it 'returns false' do
-				board.cells[0][0] = board.cells[0][1] = 'X' 
-				board.cells[0][2] = board.cells[0][3] = 'O'
-				expect(board.check_win).to be false
-			end
-		end
-	end
+    context 'when there is not a horizontal line win' do
+      it 'returns false' do
+        board.cells[0][0] = board.cells[0][1] = 'X'
+        board.cells[0][2] = board.cells[0][3] = 'O'
+        expect(board.check_win).to be false
+      end
+    end
+
+    context 'when there is a vertical line win' do
+      it 'returns true' do
+        board.cells[0][0] = board.cells[1][0] = board.cells[2][0] = board.cells[3][0] = 'X'
+        expect(board.check_win).to be true
+      end
+    end
+
+    context 'when there is not a vertical line win' do
+      it 'returns false' do
+        board.cells[0][0] = board.cells[1][0] = board.cells[3][0] = 'X'
+        board.cells[4][0] = 'O'
+        expect(board.check_win).to be false
+      end
+    end
+
+    context 'when there is a diagonal line win' do
+      it 'returns true' do
+        board.cells[0][0] = board.cells[1][1] = board.cells[2][2] = board.cells[3][3] = 'X'
+        expect(board.check_win).to be true
+      end
+    end
+
+    context 'when there is a top-left to bottom-right diagonal win' do
+      it 'returns true' do
+        board.cells[0][0] = board.cells[1][1] = board.cells[2][2] = board.cells[3][3] = 'X'
+        expect(board.check_win).to be true
+      end
+    end
+
+    context 'when there is a diagonal line win' do
+      it 'returns true' do
+        board.cells[2][3] = board.cells[3][4] = board.cells[4][5] = board.cells[5][6] = 'X'
+        expect(board.check_win).to be true
+      end
+    end
+
+    context 'when there is a bottom-left to top-right diagonal win' do
+      it 'returns true' do
+        board.cells[5][0] = board.cells[4][1] = board.cells[3][2] = board.cells[2][3] = 'O'
+        expect(board.check_win).to be true
+      end
+    end
+  end
+
+  describe '#move' do
+    let(:board) { ConnectFourBoard.new }
+
+    context 'when the cell is empty' do
+      it 'places the player symbol in the specified location' do
+        location_row = 0
+        location_column = 3
+        player_symbol = 'X'
+        expect(board.move(location_row, location_column, player_symbol)).to be true
+        expect(board.cells[location_row][location_column]).to eq(player_symbol)
+      end
+    end
+
+    context 'when the cell is not empty' do
+      it 'does not place the player symbol and returns false' do
+        location_row = 0
+        location_column = 3
+        initial_symbol = 'X'
+        new_symbol = 'O'
+        board.cells[location_row][location_column] = initial_symbol
+        expect(board.move(location_row, location_column, new_symbol)).to be false
+        expect(board.cells[location_row][location_column]).to eq(initial_symbol)
+      end
+    end
+  end
 end
 
 describe Game do
-	let(:game) { described_class.new }
+  
+  describe '#initialize' do
+  let(:game) { described_class.new }
+    context 'when using default initialization' do
+      it 'is Player 1' do
+        expect(game.player1.name).to eq('Player 1')
+      end
+      it 'is Player 2' do
+        expect(game.player2.name).to eq('Player 2')
+      end
+    end
+  end
 
-	describe '#initialize' do
-		context 'when using default initialization' do
-			it 'is Player 1' do
-				expect(game.player1).to eq('Player 1')
-			end
-			it 'is Player 2' do
-				expect(game.player2).to eq('Player 2')
-			end
-		end
+  
+  describe '#switch_players' do
+  let(:player1) { Player.new('Player 1', 'X') }
+  let(:player2) { Player.new('Player 2', 'O') }
+  let(:game) { described_class.new(player1, player2) }
+  
+    context 'when current player is player 1' do
+      it 'switches to player 2' do
+        game.switch_players
+        expect(game.current_player.name).to eq('Player 2')
+      end
+    end
 
-	end
-
+    context 'when current player is player 2' do
+      it 'switches to player 1' do
+        game.switch_players
+        game.switch_players
+        expect(game.current_player.name.name).to eq('Player 1')
+      end
+    end
+  end
 end
--- tdd_connect_four.rb
+++ tdd_connect_four.rb
@@ -2,30 +2,119 @@ class ConnectFourBoard
   attr_accessor :cells
 
   def initialize
-    @cells = Array.new(6) { Array.new(7, '')}
+    @cells = Array.new(6) { Array.new(7, 'empty')}
+  end
+
+  def display
+    puts "  0   1   2   3   4   5   6"
+    puts "+---+---+---+---+---+---+---+"
+    @cells.each do |row|
+      puts "| #{row.map { |cell| cell == 'empty' ? ' ' : cell }.join(' | ')} |"
+      puts "+---+---+---+---+---+---+---+"
+    end
+  end
+
+  def move(location_row, location_column, player_symbol)
+    if @cells[location_row][location_column] == 'empty'
+      @cells[location_row][location_column] = player_symbol
+      true
+    else
+      false
+    end
   end
 
   def check_win
+    # Check rows
     @cells.each do |row|
       row.each_cons(4) do |slice|
         return true if slice.all? { |cell| cell == 'X' } || slice.all? { |cell| cell == 'O' }
       end
     end
+
+    # Check columns
+    transposed_board = @cells.transpose
+    transposed_board.each do |column|
+      column.each_cons(4) do |slice|
+        return true if slice.all? { |cell| cell == 'X' } || slice.all? { |cell| cell == 'O' }
+      end
+    end
+    
+    # Check diagonals
+    (0..2).each do |row|
+      (0..3).each do |col|
+        return true if (0..3).all? { |i| @cells[row + i][col + i] == 'X' } || (0..3).all? { |i| @cells[row + i][col + i] == 'O' }
+      end
+    end
+
+    (0..2).each do |row|
+      (3..6).each do |col|
+        return true if (0..3).all? { |i| @cells[row + i][col - i] == 'X' } || (0..3).all? { |i| @cells[row + i][col - i] == 'O' }
+      end
+    end
+    
     false
+  end
+
+  def full?
+    cells.all? { |row| row.none? { |cell| cell == 'empty' } }
+  end
+end
+
+class Player
+  attr_accessor :name, :symbol
+
+  def initialize(name, symbol)
+    self.name = name
+    self.symbol = symbol
   end
 end
 
 class Game
-  attr_accessor :board, :player1, :player2
+  attr_accessor :board, :player1, :player2, :current_player
 
-  def initialize(player1 = 'Player 1', player2 = 'Player 2')
+  def initialize(player1 = 'Player 1', player1_symbol = "X", player2 = 'Player 2', player2_symbol = "O")
     @board = ConnectFourBoard.new
-    @player1 = player1
-    @player2 = player2
+    @player1 = Player.new(player1, player1_symbol)
+    @player2 = Player.new(player2, player2_symbol)
+    @current_player = @player1
+  end
+
+  def switch_players
+    self.current_player = current_player == @player1 ? @player2 : @player1
   end
 
   def start 
     until @board.full? || @board.check_win
+      board.display
+      puts "#{current_player.name}'s turn:"
+      puts 'Enter row (0-5):'
+      row = gets&.chomp.to_i
+      puts 'Enter column (0-6):'
+      column = gets&.chomp.to_i
+      if row.between?(0, 5) && column.between?(0, 6)
+        if board.move(row, column, current_player.symbol)
+          switch_players
+        else
+          puts 'Invalid move. Try again'
+        end
+      else
+        puts 'Invalid input. Rows and columns need to be between 0 and 2.'
+      end
+    end
+
+    board.display
+    if board.check_win
+      switch_players
+      puts "Congratulations, #{current_player.name}! You just won the game."
+    else
+      puts "It's a draw!"
     end
   end
 end
+
+
+
+# board = ConnectFourBoard.new
+# board.display
+# game = Game.new
+# game.start
